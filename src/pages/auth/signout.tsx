7e8220f39a3578a871a8b346e3577b7f1758b258
Maximilian Kaske <maximilian@kaske.org>
2021-12-29 21:11:39 +0100

    fix: signout

--- src/pages/auth/signout.tsx
+++ src/pages/auth/signout.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from "react";
-import { signOut } from "next-auth/react";
-import { useRouter } from "next/router";
+import { signOut, useSession } from "next-auth/react";
 import Layout from "@/components/common/Layout";
 import Heading from "@/components/ui/Heading";
+import { useRouter } from "next/router";
 
 const SignOut = () => {
   const router = useRouter();
+  const { status } = useSession();
 
   useEffect(() => {
-    signOut();
-    router.replace("/");
-  }, [router]);
+    if (status === "authenticated") {
+      signOut();
+      router.replace("/");
+    }
+  }, [status, router]);
+
   return (
     <Layout className="flex flex-col items-center justify-center">
       <Heading>Signing out...</Heading>
