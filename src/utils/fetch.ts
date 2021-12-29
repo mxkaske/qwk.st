1299055babf226d3ad52620f68b5acfec25b6c2d
Maximilian Kaske <maximilian@kaske.org>
2021-12-29 22:02:36 +0100

    feat: add fetch error boundary

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -4,6 +4,11 @@ const get = async (url: string, token?: string) => {
     headers: new Headers({ "Content-Type": "application/json" }),
     credentials: "same-origin",
   });
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    throw new Error("An error occurred while fetching the data.");
+  }
   return res.json();
 };
 
