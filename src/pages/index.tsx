96e54367343cfb79b2a66c84f0e2f881a0faa6dd
Maximilian Kaske <maximilian@kaske.org>
2021-12-30 00:05:18 +0100

    chore: landing page changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <Layout className="flex flex-col items-center justify-center">
       <div className="space-y-6">
-        <Heading as="h1" className="text-center">
+        <Heading as="h1" className="max-w-3xl text-center">
           <span className="underline decoration-indigo-500 decoration-4 md:decoration-[6px]">
             Main.ly
           </span>
@@ -29,8 +29,12 @@ export default function Home() {
             Linktr.ee
           </span>
         </Heading>
-        <Heading as="h3" className="text-center">
-          <Link href={`${URL}/mxkaske`}>Example @mxkaske</Link>
+        <Heading as="h4" className="max-w-sm mx-auto text-center">
+          Curious about how it looks? Check out{" "}
+          <Link href={`${URL}/mxkaske`} className="italic">
+            @mxkaske
+          </Link>
+          .
         </Heading>
       </div>
     </Layout>
