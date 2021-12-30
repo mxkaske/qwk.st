0a39e06bc126240212c71aa3716084cfff1fceed
Maximilian Kaske <maximilian@kaske.org>
2021-12-30 10:20:21 +0100

    fix: 404 layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,13 +2,18 @@ import Text from "@/components/ui/Text";
 import Link from "@/components/ui/Link";
 import React from "react";
 import Layout from "@/components/common/Layout";
+import Heading from "@/components/ui/Heading";
 
 const NotFound = () => {
   return (
-    <Layout>
-      <Text>Not Found</Text>
-      <Text>Want this to be your username?</Text>
-      <Link href="/auth/signin">Sign in now!</Link>
+    <Layout className="flex flex-col items-center justify-center text-center">
+      <Heading as="h1">Not Found</Heading>
+      <div>
+        <Text className="text-gray-600 dark:text-gray-400">
+          Want this to be your username?
+        </Text>
+        <Link href="/auth/signin">Sign in now</Link>
+      </div>
     </Layout>
   );
 };
