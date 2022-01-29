5db70f487ba812121823757998c0b994ee78b425
Maximilian Kaske <maximilian@kaske.org>
2022-01-29 14:23:22 +0100

    chore: add middleware

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  // Get hostname (e.g. vercel.com, test.vercel.app, etc.)
+  const hostname = req.headers.get("host");
+  // If localhost, assign the host value manually
+  // If prod, get the custom domain/subdomain value by removing the root URL
+  // (in the case of "test.vercel.app", "vercel.app" is the root URL)
+
+  const url =
+    process.env.VERCEL_ENV === "production" ? "qwk.st" : "staging.qwk.st";
+
+  const currentHost =
+    process.env.NODE_ENV == "production"
+      ? hostname?.replace(`.${url}`, "")
+      : process.env.CURR_HOST;
+
+  console.log({ url, currentHost, hostname, pathname });
+
+  // Prevent running if-statements if currentHost is invalid.
+  if (!currentHost || currentHost === "" || currentHost === url) {
+    return NextResponse.next();
+  }
+
+  // Prevent security issues – users should not be able to canonically access
+  // the pages/sites folder and its respective contents. This can also be done
+  // via rewrites to a custom 404 page
+  if (pathname.startsWith(`/_sites`)) {
+    return new Response(null, { status: 404 });
+  }
+
+  if (pathname.startsWith("/sites")) {
+    // rewrite to the current hostname under the pages/sites folder
+    // the main logic component will happen in pages/sites/[site]/index.tsx
+    return NextResponse.rewrite(`/_sites/${currentHost}`);
+  }
+
+  /**
+   * EXAMPLE
+   */
+
+  if (
+    !pathname.includes(".") && // exclude all files in the public folder
+    !pathname.startsWith("/api") // exclude all API routes
+  ) {
+    // rewrite to the current hostname under the pages/sites folder
+    // the main logic component will happen in pages/sites/[site]/index.tsx
+    return NextResponse.rewrite(`/_sites/${currentHost}`);
+  }
+}
