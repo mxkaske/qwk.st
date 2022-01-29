85a3d03f480d0693c1e06e4d2fcab8fb5c4dade0
Maximilian Kaske <maximilian@kaske.org>
2022-01-29 18:19:08 +0100

    test: nextjs redirect

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com", "avatars.githubusercontent.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:slug",
+        destination: "/_sites/:slug",
+        permanent: true,
+      },
+    ];
+  },
 };
